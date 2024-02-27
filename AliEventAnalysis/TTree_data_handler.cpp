b87bb526f0a6ec6c6c6bd092525398fd4cbb0314
Varlamov Antony <Varlamov.AM@phystech.edu>
2024-02-27 13:09:00 +0300

    New version. Handle pythia SoftQCD data before Geant

--- AliEventAnalysis/TTree_data_handler.cpp
+++ AliEventAnalysis/TTree_data_handler.cpp
@@ -34,65 +34,162 @@ int main(int argc, char* argv[]){
         source_data_path = TString(argv[1]);
     }
     
-    TFile* output = new TFile(Form("%selectron_positron_gamma_4momentum.root", source_data_path.Data()), "RECREATE");
+    TChain* chain = new TChain("AliEvent_data");
+
+    for (int i = 0; i <= 18; ++i){
+       FILE* fp = fopen(Form("%sAliEvent_class_data_%d.root", source_data_path.Data(), i), "rb");
+       if (fp != NULL){
+           chain->Add(Form("%sAliEvent_class_data_%d.root", source_data_path.Data(), i));
+       }
+    }
+
+    std::cout << chain->GetEntries() << "\n";
+
+    AliEvent* event = nullptr;
+    
+    chain->SetBranchAddress("AliEvent", &event);
+
+    TFile* output = new TFile("output_Soft_QCD_before_Geant.root", "RECREATE");
     output->cd();
-    TTree* true_chic_daughters_for_GEANT4 = new TTree("signal_event_electron_positron_gamma_from_chic_with_branchings", "signal_event_electron_positron_gamma_from_chic_with_branchings");
 
-    double branching = 0;
-    double gamma_px = 0;
-    double gamma_py = 0;
-    double gamma_pz = 0;
-    double gamma_p0 = 0;
-    double electron_px = 0;
-    double electron_py = 0;
-    double electron_pz = 0;
-    double electron_p0 = 0;
-    double positron_px = 0;
-    double positron_py = 0;
-    double positron_pz = 0;
-    double positron_p0 = 0;
+	const int idK0L          =  130;
+	const int idn            =  2112;
+	const int idp            =  2212;
+	const int idPic          =  211;
+	const int idPin          =  111;
+	const int idKc           =  321;
+    
+    TH2D* electrons_pt_eta = new TH2D("electrons_pt_eta", "electrons_pt_eta", 1000., 0., 20., 1000, -2.5, 2.5);
+    TH2D* positrons_pt_eta = new TH2D("positrons_pt_eta", "positrons_pt_eta", 1000., 0., 10.,  1000, -2.5, 2.5);
+    TH2D* muons_pt_eta = new TH2D("muons_pt_eta", "muons_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* antimuons_pt_eta = new TH2D("antimuons_pt_eta", "antimuons_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* gamma_pt_eta = new TH2D("gamma_pt_eta", "gamma_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* proton_pt_eta = new TH2D("proton_pt_eta", "proton_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* kaon_pt_eta = new TH2D("kaon_pt_eta", "kaon_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* pion_pt_eta = new TH2D("pion_pt_eta", "pion_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* kaonl_pt_eta = new TH2D("kaonl_pt_eta", "kaonl_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    TH2D* neutron_pt_eta = new TH2D("neutron_pt_eta", "neutron_pt_eta", 1000., 0., 10., 1000, -2.5, 2.5);
+    electrons_pt_eta->Sumw2();
+    positrons_pt_eta->Sumw2();
+    muons_pt_eta->Sumw2();
+    antimuons_pt_eta->Sumw2();
+    gamma_pt_eta->Sumw2();
+    proton_pt_eta->Sumw2();
+    kaon_pt_eta->Sumw2();
+    pion_pt_eta->Sumw2();
+    kaonl_pt_eta->Sumw2();
+    neutron_pt_eta->Sumw2();
 
-    true_chic_daughters_for_GEANT4->Branch("event_branching", &branching);
+    TH2D* electrons_p_e = new TH2D("electrons_p_e", "electrons_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* positrons_p_e = new TH2D("positrons_p_e", "positrons_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* muons_p_e = new TH2D("muons_p_e", "muons_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* antimuons_p_e = new TH2D("antimuons_p_e", "antimuons_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* gamma_p_e = new TH2D("gamma_p_e", "gamma_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* proton_p_e = new TH2D("proton_p_e", "proton_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* kaon_p_e = new TH2D("kaon_p_e", "kaon_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* pion_p_e = new TH2D("pion_p_e", "pion_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* kaonl_p_e = new TH2D("kaonl_p_e", "kaonl_p_e", 1000, 0., 20., 1000., 0., 20.);
+    TH2D* neutron_p_e = new TH2D("neutron_p_e", "neutron_p_e", 1000, 0., 20., 1000., 0., 20.);
 
-    true_chic_daughters_for_GEANT4->Branch("gamma_px", &gamma_px);
-    true_chic_daughters_for_GEANT4->Branch("gamma_py", &gamma_py);
-    true_chic_daughters_for_GEANT4->Branch("gamma_pz", &gamma_pz);
-    true_chic_daughters_for_GEANT4->Branch("gamma_p0", &gamma_p0);
 
-    true_chic_daughters_for_GEANT4->Branch("electron_px", &electron_px);
-    true_chic_daughters_for_GEANT4->Branch("electron_py", &electron_py);
-    true_chic_daughters_for_GEANT4->Branch("electron_pz", &electron_pz);
-    true_chic_daughters_for_GEANT4->Branch("electron_p0", &electron_p0);
 
-    true_chic_daughters_for_GEANT4->Branch("positron_px", &positron_px);
-    true_chic_daughters_for_GEANT4->Branch("positron_py", &positron_py);
-    true_chic_daughters_for_GEANT4->Branch("positron_pz", &positron_pz);
-    true_chic_daughters_for_GEANT4->Branch("positron_p0", &positron_p0);
+    for (int i = 0; i < chain->GetEntries(); ++i){
 
-  	true_chic_daughters_for_GEANT4->SetAutoSave(0);
+        chain->GetEntry(i);
 
-    for (int i = 0; i < 50; ++i){
-        for (int j = 0; j < 10000; ++j){
-            if (j % 1000 == 0){
-                printf("Handled events:%10d\n", j);
+        if (i % 10000 == 0){
+            printf(Form("Handled events: %07d of %07d\n", i, chain->GetEntries()));
+        }
+
+        bool signal_event_in_data = event->signal_event_in_data;
+        std::vector<AliParticle> chic = event->chic;
+        std::vector<AliParticle> Jpsi = event->Jpsi;
+        std::vector<AliParticle> signal_electrons = event->signal_electrons;
+        std::vector<AliParticle> signal_positrons = event->signal_positrons;
+        std::vector<AliParticle> signal_photons = event->signal_photons;
+        
+        std::vector<AliParticle> electrons = event->electrons;
+        std::vector<AliParticle> positrons = event->positrons;
+        std::vector<AliParticle> muons = event->muons;
+        std::vector<AliParticle> antimuons = event->antimuons;
+        std::vector<AliParticle> photons = event->photons;
+        std::vector<AliParticle> charged = event->charged;
+        std::vector<AliParticle> neutral = event->neutral;       
+
+        for (auto elec : electrons){
+            electrons_pt_eta->Fill(elec.FMomentum().Pt(), elec.FMomentum().Eta());
+            electrons_p_e->Fill(elec.FMomentum().P(), elec.FMomentum().E());
+        }
+
+        for (auto posi : positrons){
+            positrons_pt_eta->Fill(posi.FMomentum().Pt(), posi.FMomentum().Eta());
+            positrons_p_e->Fill(posi.FMomentum().P(), posi.FMomentum().E());
+        }
+
+        for (auto muon : muons){
+            muons_pt_eta->Fill(muon.FMomentum().Pt(), muon.FMomentum().Eta());
+            muons_p_e->Fill(muon.FMomentum().P(), muon.FMomentum().E());
+        }
+
+        for (auto antimuon : antimuons){
+            antimuons_pt_eta->Fill(antimuon.FMomentum().Pt(), antimuon.FMomentum().Eta());
+            antimuons_p_e->Fill(antimuon.FMomentum().P(), antimuon.FMomentum().E());
+        }
+
+        for (auto gamma : photons){
+            gamma_pt_eta->Fill(gamma.FMomentum().Pt(), gamma.FMomentum().Eta());
+            gamma_p_e->Fill(gamma.FMomentum().P(), gamma.FMomentum().E());
+        }
+
+        for (auto charg : charged){
+            if (charg.id == idp){
+                proton_pt_eta->Fill(charg.FMomentum().Pt(), charg.FMomentum().Eta());
+                proton_p_e->Fill(charg.FMomentum().P(), charg.FMomentum().E());
             }
-            electron_p0 = 0;
-            electron_px = 0;
-            electron_py = 0;
-            electron_pz = 0;
-
-            positron_p0 = 0;
-            positron_px = 0;
-            positron_py = 0;
-            positron_pz = 0;
-
-            gamma_p0 = i * 0.1;
-            gamma_px = 0;
-            gamma_py = 0;
-            gamma_pz = i * 0.1;
+            if (charg.id == idKc){
+                kaon_pt_eta->Fill(charg.FMomentum().Pt(), charg.FMomentum().Eta());
+                kaon_p_e->Fill(charg.FMomentum().P(), charg.FMomentum().E());
+            }
+            if (charg.id == idPic){
+                pion_pt_eta->Fill(charg.FMomentum().Pt(), charg.FMomentum().Eta());
+                pion_p_e->Fill(charg.FMomentum().P(), charg.FMomentum().E());
+            }
+        }
+        for (auto neutr : neutral){
+            if (neutr.id == idn){
+                neutron_pt_eta->Fill(neutr.FMomentum().Pt(), neutr.FMomentum().Eta());
+                neutron_p_e->Fill(neutr.FMomentum().P(), neutr.FMomentum().E());
+            }
+            if (neutr.id == idK0L){
+                kaonl_pt_eta->Fill(neutr.FMomentum().Pt(), neutr.FMomentum().Eta());
+                kaonl_p_e->Fill(neutr.FMomentum().P(), neutr.FMomentum().E());
+            }
         }
     }
-    true_chic_daughters_for_GEANT4->Write();
-    output->Close();
+
+    electrons_pt_eta->Write();
+    positrons_pt_eta->Write();
+    muons_pt_eta->Write();
+    antimuons_pt_eta->Write();  
+    gamma_pt_eta->Write();
+    proton_pt_eta->Write();
+    kaon_pt_eta->Write();
+    pion_pt_eta->Write();
+    neutron_pt_eta->Write();
+    kaonl_pt_eta->Write();
+
+    electrons_p_e->Write();
+    positrons_p_e->Write();
+    muons_p_e->Write();
+    antimuons_p_e->Write();  
+    gamma_p_e->Write();
+    proton_p_e->Write();
+    kaon_p_e->Write();
+    pion_p_e->Write();
+    neutron_p_e->Write();
+    kaonl_p_e->Write();
     
+    output->Close();
+
+    return 0;
 }
